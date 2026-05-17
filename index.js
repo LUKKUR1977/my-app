637a3f58352b27dc8ca66a0077571417002d952b
LK <lk@iphone-2.home>
2026-05-17 18:46:35 +0200

    FIX ENTRY FILE

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require("express");
 
 const app = express();
 
-// ✅ KLUCZOWE
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-// ✅ root — MUSI odpowiadać
 app.get("/", (req, res) => {
-  res.send("✅ DZIAŁA 100%");
+  res.send("✅ DZIAŁA FINAL");
 });
 
-// ✅ start
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 SERVER RUNNING ON", PORT);
 });
